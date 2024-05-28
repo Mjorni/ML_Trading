a1b87ac7f4a6c460c7ce68d6d0f5df0b5c61bb30
poko <71909795+Mjorni@users.noreply.github.com>
2024-05-28 21:06:26 +0300

    back save

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ from tkinter import *
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 import os
-import nkot
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 
+import nkot
+
 root = Tk()
 root.title("PokoAI")
 root.geometry("650x400") 
 
-path_to_file = str
+path_to_file = ''
 
 def get_path_to_file():
+    text_path.delete(0, END)
     try:
         path_to_file = askopenfilename(title = "Выберите csv файл!", filetypes=[("CSV files", "*.csv")])
         if path_to_file:
@@ -23,8 +25,9 @@ def get_path_to_file():
             text_path.insert(0, path_to_file)
             return path_to_file
     except:
-        text_path.insert(0, os.getcwd() + "/LOGI.csv")
-        return os.getcwd() + "/LOGI.csv"
+        path_to_file = os.getcwd().replace("\\", "/") + "/LOGI.csv"
+        text_path.insert(0, path_to_file)
+        return path_to_file.replace("\\", "/")
 
 
 butt_choise_file = ttk.Button(root, text = "Выбрать файл", command=get_path_to_file)
--- nkot.py
+++ nkot.py
@@ -3,9 +3,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
+from main import path_to_file
 
-df = pd.read_csv("LOGI.csv", parse_dates=["Date"])
-
+df = pd.read_csv(path_to_file, parse_dates=["Date"])
+print(df)
 def get_prewiev(df):
     plt.plot(df[["Open", "Close", "Low", "High"]], label = ["Open", "Close", "Low", "High"], linewidth = 0.5)
     plt.legend()
