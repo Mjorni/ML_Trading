d10ba98535d8f5ee143ae3e37262b1db6e511cd9
poko <71909795+Mjorni@users.noreply.github.com>
2024-05-29 15:25:12 +0300

    v1.0

--- main.py
+++ main.py
@@ -6,41 +6,151 @@ import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
-
+from matplotlib.figure import Figure
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential # type: ignore
+from tensorflow.keras.layers import Dense, LSTM, Dropout, Activation # type: ignore
+from threading import Thread
 import nkot
 
 root = Tk()
 root.title("PokoAI")
-root.geometry("650x400") 
+root.geometry("1200x800") 
 
-path_to_file = ''
+file_path = None
+df = None
 
 def get_path_to_file():
+    """Запись пути до выбранного файла"""
+    global file_path
+    file_path = askopenfilename(title = "Необходимо выбрать csv файл", filetypes = [("CSV files", "*.csv")])
     text_path.delete(0, END)
-    try:
-        path_to_file = askopenfilename(title = "Выберите csv файл!", filetypes=[("CSV files", "*.csv")])
-        if path_to_file:
-            choise_file = os.path.splitext(path_to_file)[1].lower()
-        if choise_file == ".csv":
-            text_path.insert(0, path_to_file)
-            return path_to_file
-    except:
-        path_to_file = os.getcwd().replace("\\", "/") + "/LOGI.csv"
-        text_path.insert(0, path_to_file)
-        return path_to_file.replace("\\", "/")
+    if file_path:
+        text_path.insert(0, file_path)
+    else:
+        text_path.insert(0, "LOGI.csv")
+        file_path = "LOGI.csv"
+        
+
+def get_previev():
+    """Функция отображения графа"""
+    global df
+    df = pd.read_csv(file_path, parse_dates=["Date"])
+
+    ax.plot(df[["Open", "Close", "Low", "High"]], label = ["Open", "Close", "Low", "High"], linewidth = 0.5)
+    ax.legend()
+
+    canvas.draw()
+    canvas.get_tk_widget().pack(side = LEFT)
+    get_all_col([column for column in df.drop(['Date', 'Adj Close'], axis = 1)])
+
+
+def get_all_col(df):
+    """отображение кнопок с названиями столбцов """
+    choise_entry.pack(anchor = N)
+    btt_start_train.pack(anchor = N)
+    new_plot.pack(anchor = N)
+    for i in df:
+        butt = ttk.Button(text = i, command = lambda x = i:choise_coll(x))
+        butt.pack(anchor = N)
+    
+
+def choise_coll(comm):
+    choise_entry.delete(0, END)
+    choise_entry.insert(0, comm)
+    
+
+def new_graph():
+    canvas.get_tk_widget().pack_forget()
+    ax.clear()
+    print(df[choise_entry.get()])
+    ch_col = df[choise_entry.get()]
+    plt.title(choise_entry.get())
+    ax.plot(ch_col)
+
+    canvas.draw()
+    canvas.get_tk_widget().pack(side = LEFT)
 
 
 butt_choise_file = ttk.Button(root, text = "Выбрать файл", command=get_path_to_file)
-butt_choise_file.grid(row = 5, column = 1, padx = 10, pady = 10)
+butt_choise_file.pack(anchor = NW, padx = 10, pady = 10)
 
-butt_start = ttk.Button(root, text = "Начать обучение")
-butt_start.grid(row = 5, column = 2)
+butt_start = ttk.Button(root, text = "Начать обучение", command = get_previev)
+butt_start.pack(anchor = NW, padx = 10)
 
 
-text_path = ttk.Entry(root)
-text_path.place(x = 10, y = 45, width = 450)
+text_path = ttk.Entry(root, width = 75)
+text_path.pack(anchor = NW, padx = 10, pady = 10)
 
-#figure = plt.subplot()
+choise_entry = ttk.Entry(root, width = 23)
 
+fig = plt.figure(figsize=(8, 6), dpi = 100)
+ax = fig.add_subplot()
+canvas = FigureCanvasTkAgg(fig, master = root) #холст
+new_plot = ttk.Button(root, text = "Отобразить новый график", command = new_graph)
+
+
+
+"""РАЗДЕЛ С ОБУЧЕНИЕМ"""
+lb = 10 
+val = 0.9
+
+model = Sequential([
+            LSTM(256, input_shape = (lb, 1), return_sequences = True),
+            LSTM(10),
+            Dense(1)
+        ])
+model.compile(optimizer = 'adam', loss = 'mse')
+import tracemalloc
+
+#   Разделение данных на тестовые и тренеровочные
+def start_train():
+    #Разбиение на тестовые и тренеровочные данные
+
+    x,y = xyarray(stab_data(choise_entry.get()))
+
+    x_train, x_test = x[:int(x.shape[0] * val)], x[int(x.shape[0] * val):]
+    y_train, y_test = y[:int(x.shape[0] * val)], y[int(x.shape[0] * val):]
+    print(f"x train -> {x_train.shape}  \ny train -> {y_train.shape}  \nx test  -> {x_test.shape} \ny test  -> {y_test.shape}")
+    get_res(x_train, y_train, x_test, y_test)
+    
+
+def get_res(x_train, y_train, x_test, y_test):
+    x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))
+    history = model.fit(x_train, y_train, epochs = 5, shuffle=False)
+    model.evaluate(x_test, y_test)
+    result_predict = model.predict(x_test)
+    canvas.get_tk_widget().pack_forget()
+    ax.clear()
+    plt.title(f"Обученная модель на {choise_entry.get()}")
+    ax.plot(result_predict, label = 'x - Предсказанные данные')
+    ax.plot(y_test, label = 'y - Истиные данные')
+    ax.legend()
+    canvas.draw()
+    canvas.get_tk_widget().pack(side = LEFT)
+
+
+def stab_data(name_col):
+    "Нормализация данных"
+    column = df[name_col].astype('float')
+    #train = column[:int(len(column))] #не обязательно. ничего не делает, в случае чего, позже можно будет удалить
+    scale_col = MinMaxScaler()
+    scale_col.fit(column.values.reshape(-1, 1))
+    column = scale_col.transform(column.values.reshape(-1, 1))
+    return column
+
+def xyarray(data):
+    "Разбиение массива на x y "
+    x, y = [], []
+    for i in range(len(data) - lb - 1):
+        x.append(data[i:(i + lb), 0])
+        y.append(data[(i + lb), 0])
+    return np.array(x), np.array(y)
+
+
+
+btt_start_train = ttk.Button(root, text = "Начать обучение", command = start_train)
 
 root.mainloop()
